0bad91e9807f062d6f286f18e3dd0a747d095d28
duanfangwei <duanfangwei@dobest.com>
2026-01-08 18:24:16 +0800

    修复变量引用问题

--- plugins/in_tail_http_filter/tail_http_filter.c
+++ plugins/in_tail_http_filter/tail_http_filter.c
@@ -458,8 +458,8 @@ int is_file_allowed(const char *file_path, struct flb_tail_http_filter_config *c
             return FLB_TRUE;
         }
     }
-    flb_plg_debug(ctx->tail_config->ins, "file %s not match pattern %s", file_path, entry->pattern);
-
+    flb_plg_debug(ctx->tail_config->ins, "file %s not match any pattern", file_path);
+    
     return FLB_FALSE;
 }
 
